8df1fd6dbf56b5a971f54274c0db3ad4edf2b841
Sukh-1995 <ersukh95@gmail.com>
2025-05-07 12:18:35 -0400

    shortcut using props

--- src/App.jsx
+++ src/App.jsx
@@ -45,21 +45,9 @@ function App() {
             description={CORE_CONCEPTS[0].description}
             image={CORE_CONCEPTS[0].image} 
           />
-          <CoreConcepts
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
-          />
-          <CoreConcepts 
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
-          />
-          <CoreConcepts 
-            title={CORE_CONCEPTS[3].title}
-            description={CORE_CONCEPTS[3].description}
-            image={CORE_CONCEPTS[3].image}
-          />
+          <CoreConcepts {...CORE_CONCEPTS[1]}/>
+          <CoreConcepts {...CORE_CONCEPTS[2]}/>
+          <CoreConcepts {...CORE_CONCEPTS[3]}/>
         </ul>
         </section>
         
