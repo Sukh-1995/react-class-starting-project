5614c7c1b46d71014a6e3d5d1cd011134f5934d6
Sukh-1995 <ersukh95@gmail.com>
2025-05-14 14:17:18 -0400

    new component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButtton from './components/TabButton.jsx';
 
 function App() {
   return (
@@ -21,6 +22,15 @@ function App() {
         </ul>
         </section>
         
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButtton>Components</TabButtton>
+            <TabButtton>JSX</TabButtton>
+            <TabButtton>Props</TabButtton>
+            <TabButtton>State</TabButtton>
+          </menu>
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButtton({children}) {
+    return(
+     <li>
+        <button>{children}</button>
+        </li>
+    );
+}
